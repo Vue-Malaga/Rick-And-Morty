eccb7d15d5f20f798668465d4c9a117abdadd248
Jesus Narbona <jesusnarbona2001@gmail.com>
2023-01-02 23:56:51 +0100

    feat: added new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Characters from '../components/Characters.vue'
+import CharacterInfo from '../components/CharacterInfo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,12 @@ const router = createRouter({
     {
       path:'/characters',
       component: Characters
+    },
+    {
+      name:'characterInfo',
+      path:'/characters/:id',
+      component: CharacterInfo,
+      props:true
     },
     {
       path:'/:pathMatch(.*)*',
